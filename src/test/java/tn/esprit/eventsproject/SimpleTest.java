0324153eca4c8cafa26d1c025484ea9e3e01d09f
Iheb Jdey <jdey.iheb@esprit.tn>
2025-09-29 06:03:14 +0100

    add simple JUnit test

---
+++ src/test/java/tn/esprit/eventsproject/SimpleTest.java
@@ -0,0 +1,12 @@
+package tn.esprit.eventsproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTest {
+    @Test
+    void alwaysTrue() {
+        assertTrue(true);
+    }
+}
